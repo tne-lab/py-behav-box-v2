a013bb0fb3263f7876b4fba1a85c29f06f5d64ea
theonlydvr <theonlydvr@gmail.com>
2022-12-12 14:54:49 -0600

    Start Whisker with Pybehave

--- source/Sources/WhiskerLineSource.py
+++ source/Sources/WhiskerLineSource.py
@@ -1,24 +1,48 @@
 import socket
+import subprocess
 import threading
+import time
 import traceback
+
+import win32gui
 
 from Components.Component import Component
 from Sources.Source import Source
 
 
+IsWhiskerRunning = False
+
+
+def look_for_program(hwnd, program_name):
+    global IsWhiskerRunning
+    if program_name in win32gui.GetWindowText(hwnd):
+        win32gui.CloseWindow(hwnd)  # Minimize Window
+        IsWhiskerRunning = True
+
+
 class WhiskerLineSource(Source):
 
-    def __init__(self, address='localhost', port=3233):
+    def __init__(self, address='localhost', port=3233, whisker_path=r"C:\Program Files (x86)\WhiskerControl\WhiskerServer.exe"):
         super(WhiskerLineSource, self).__init__()
         try:
-            self.available = True
-            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            self.client.connect((address, int(port)))
-            self.running = threading.Event()
-            rt = threading.Thread(target=lambda: self.read())
-            rt.start()
-            self.vals = {}
+            win32gui.EnumWindows(look_for_program, 'WhiskerServer')
+            if not IsWhiskerRunning:
+                ws = whisker_path
+                window = subprocess.Popen(ws)
+                time.sleep(2)
+                print("WHISKER server started", window)
+                win32gui.EnumWindows(look_for_program, 'WhiskerServer')
+            if IsWhiskerRunning:
+                self.available = True
+                self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                self.client.connect((address, int(port)))
+                self.running = threading.Event()
+                rt = threading.Thread(target=lambda: self.read())
+                rt.start()
+                self.vals = {}
+            else:
+                self.available = False
         except:
             traceback.print_exc()
             self.available = False
@@ -26,6 +50,7 @@ class WhiskerLineSource(Source):
     def read(self):
         while not self.running.is_set():
             msg = self.client.recv(4096).decode()
+            print(msg)
             if msg.startswith('Event:'):
                 self.vals[msg[:-1].split(' ')[1]] = not self.vals[msg[:-1].split(' ')[1]]
         self.client.send(b'LineRelinquishAll\n')
